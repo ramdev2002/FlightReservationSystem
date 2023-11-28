3c0f2e1e2de8bcd0fd890456a903fc5e872abc20
ramdev2002 <ramdevtamang2002@gmail.com>
2023-11-28 21:32:58 +0545

    initial commit

---
+++ Air_reservation/src/main/java/com/javaproject/air_reservation/AirReservationApplication.java
@@ -0,0 +1,13 @@
+package com.javaproject.air_reservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirReservationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AirReservationApplication.class, args);
+    }
+
+}
---
+++ Air_reservation/src/test/java/com/javaproject/air_reservation/AirReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaproject.air_reservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirReservationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
